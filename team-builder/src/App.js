7e7ae6b6aa2c6fa3ede7f1a5de512c591e9bde6f
unknown <nickh3592@gmail.com>
2021-08-17 09:41:22 -0400

    finished the form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({ username: "", email: "", role: "" })
+
+  const handleChange= event => {
+    setUser({...user, [event.target.name]: event.target.value })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefualt();
+    console.log(user.username)
+    console.log(user.email)
+    console.log(user.role)
+    setUser({ username: '', email: '', role: '' })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={event => handleSubmit(event)}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <label>
+          email:
+          <input
+            type="text"
+            name="email"
+            value={user.email}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <label>
+          role:
+          <input
+            type="text"
+            name="role"
+            value={user.role}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <button>Submit!</button>
+      </form>
     </div>
   );
 }
